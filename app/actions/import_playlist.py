037d22f2e1c436185fc212e3692551212a08428b
jliottard <59571132+jliottard@users.noreply.github.com>
2024-02-13 20:09:25 +0100

    add custom and several playlist import possibility

--- app/actions/import_playlist.py
+++ app/actions/import_playlist.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 from app import configuration
 from audio.audio_player import AudioPlayer
@@ -7,10 +7,23 @@ from audio.playlist import Playlist
 
 def import_defaut_playlist(player: AudioPlayer) -> Tuple[Playlist, AudioPlayer]:
     """ Check playlist audio files are available locally and generate a new playlist and player"""
+    return import_playlist([configuration.DEFAULT_PROFILE], player)
+
+def import_playlist(args: List[str], player: AudioPlayer) -> Tuple[Playlist, AudioPlayer]:
+    ''' Get playlist contents from the args' profile and load the player for the audios
+    @param: args: List[str]: contains in the second argument that must be the name of the profile from the configuration file
+    @return the loaded playlist and the player or None if the audios cannot be loaded
+    '''
+    if len(args) < 2:
+        return None
+    profile = str(args[1])
+    playlist_path = configuration.get_playlist_file_path(profile)
+    playlist = Playlist()
+    playlist.audios = audio_loader.import_playlist_audios(
+        playlist_file_absolute_path=playlist_path,
+        playlist_profile=profile
+    )
     if player is not None:
         player.stop()
-    playlist_path = configuration.get_playlist_file_path()
-    playlist = Playlist()
-    playlist.audios = audio_loader.import_playlist_audios(playlist_file_absolute_path=playlist_path)
     player = AudioPlayer(playlist)
     return playlist, player
--- test/test_configuration.py
+++ test/test_configuration.py
@@ -4,7 +4,7 @@ def test_check_required_files_from_configuration_exist():
     assert configuration.check_required_files_from_configuration_exist() is True
 
 def test_get_audios_directory_path_is_string():
-    assert isinstance(configuration.get_audios_directory_path(), str)
+    assert isinstance(configuration.get_audios_directory_path(configuration.DEFAULT_PROFILE), str)
 
 def test_get_playlist_file_path_is_string():
-    assert isinstance(configuration.get_playlist_file_path(), str)
+    assert isinstance(configuration.get_playlist_file_path(configuration.DEFAULT_PROFILE), str)
