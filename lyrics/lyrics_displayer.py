7db66a3574607ebfe060b222451015e1a59e541f
jliottard <59571132+jliottard@users.noreply.github.com>
2024-05-17 00:18:18 +0200

    remove dash of lyrics display

--- lyrics/lyrics_displayer.py
+++ lyrics/lyrics_displayer.py
@@ -63,7 +63,7 @@ class LyricsDisplayer:
                 lyric_text: pylrc.classes.Lyrics = self.get_lyric_text(maybe_base_audio)
                 self.displayed_lyric_audio = maybe_base_audio
                 last_lyric = lyric_text[-1]
-                user_interface.request_output_to_user(f"Info:\n - \"{self.displayed_lyric_audio.name}\" audio's lyrics:")
+                user_interface.request_output_to_user(f"Info: \"{self.displayed_lyric_audio.name}\" audio's lyrics:")
                 for lyric_line in lyric_text:
                     if self.are_lyrics_on:
                         maybe_progress_time_in_sec = self.player.get_audio_progress_time_in_sec()
@@ -79,7 +79,7 @@ class LyricsDisplayer:
                         if not self.are_lyrics_on or has_audio_changed:
                             break
                         if self.player.is_playing():
-                            user_interface.request_output_to_user("\t-" + lyric_line.text)
+                            user_interface.request_output_to_user("\t" + lyric_line.text)
                     else:
                         break
             else:
