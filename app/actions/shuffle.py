de613df59895fd3d32367819b124862443133cdd
jliottard <59571132+jliottard@users.noreply.github.com>
2024-05-28 20:00:54 +0200

    fix shuffle, replay the current audio at the timing before shuffle and reduce time of get played audio method

--- app/actions/shuffle.py
+++ app/actions/shuffle.py
@@ -1,14 +1,19 @@
 from app.interface import Interface
+from audio.audio import Audio
 from audio.audio_player import AudioPlayer
 
 def shuffle_playlist(player: AudioPlayer, user_interface: Interface):
-    """Rearrange the order of the player's playlist
+    """Rearrange the order of the player's playlist, pause and resume the played audio
     @param player: AudioPlayer
     @param user_interface: Interface
     """
-    current_audio = player.get_playing_audio()
+    maybe_current_audio: Audio | None = player.get_playing_audio()
+    maybe_time_progression_in_sec: float | None = player.get_audio_progress_time_in_sec()
+    print("audio", maybe_current_audio, "time", maybe_time_progression_in_sec)
     player.stop()
     user_interface.request_output_to_user("Info: Shuffling the playlist.")
     player.shuffle()
-    index = player.get_index_of_audio(current_audio)
-    player.play_audio_at_index(index)
+    if maybe_current_audio is not None:
+        player.play_audio_at_index(player.get_index_of_audio(maybe_current_audio))
+    if maybe_time_progression_in_sec is not None:
+        player.set_current_audio_time(int(maybe_time_progression_in_sec))
