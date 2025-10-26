a94dd2bc3cb5ecd51e762414143056f032c2cfce
jliottard <59571132+jliottard@users.noreply.github.com>
2025-10-26 14:17:10 +0100

    add: adapt interface mock for muting methods and message priorit

--- test/interface_mock.py
+++ test/interface_mock.py
@@ -1,7 +1,9 @@
 
 from audio.audio_player import AudioPlayer
+from app.interface import Interface
+from app.message_priority import MessagePriority
 
-class InterfaceMock:
+class InterfaceMock(Interface):
     """Mock class responsible for controlling input and ouput from and to user for multhreading access"""
 
     def __init__(self):
@@ -37,6 +39,7 @@ class InterfaceMock:
     def book_user_input(self):
         """Acquire the lock for accessing user's input"""
         pass
+    
     def free_user_input(self):
         """Acquire the lock for accessing user's input"""
         pass
@@ -48,7 +51,7 @@ class InterfaceMock:
         """
         pass
 
-    def request_output_to_user(self, output: str):
+    def request_output_to_user(self, output: str, priority: MessagePriority):
         """Place to interface's next requested action to user_interface.request_output_to_user the <output>
         @param output: str the message to user_interface.request_output_to_user to the user
         """
@@ -58,4 +61,12 @@ class InterfaceMock:
         """Refresh the terminal to display current status information
         @param player: AudioPlayer
         """
+        pass
+    
+    def mute_message(self, priority: MessagePriority):
+        ''' Mute the messages from the given priority '''
+        pass
+
+    def unmute_message(self, priority: MessagePriority):
+        ''' Unmute the messages from the given priority '''
         pass
